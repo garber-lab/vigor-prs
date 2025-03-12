9fcc9bdf34c3697f85a22edc3657123534f1d6af
Genevieve Roberts <gen@gen-omix.co>
2025-03-11 20:00:57 -0400

    add the "chr" prefix to chrom names in vcf

--- impute_SNPs/0_filter_snps_preimputation.sh
+++ impute_SNPs/0_filter_snps_preimputation.sh
@@ -13,7 +13,6 @@ module load htslib
 # Ensure output directory exists
 mkdir -p /home/genevieve.roberts-umw/pre_imputation_vcf
 
-# Filter based on HWE
 for chr in {1..23}; do
   plink \
     --bfile "/pi/manuel.garber-umw/human/VIGOR/tj/ibd/run_1/all_samples_ibd" \
@@ -23,10 +22,24 @@ for chr in {1..23}; do
     --recode vcf \
     --out "/home/genevieve.roberts-umw/pre_imputation_vcf/chr${chr}_all_samples_ibd_hwe_0.001"
 
-  # Check if PLINK output exists before attempting gzip
-  if [[ -f "/home/genevieve.roberts-umw/pre_imputation_vcf/chr${chr}_all_samples_ibd_hwe_0.001.vcf" ]]; then
-    bgzip "/home/genevieve.roberts-umw/pre_imputation_vcf/chr${chr}_all_samples_ibd_hwe_0.001.vcf"
-    tabix "/home/genevieve.roberts-umw/pre_imputation_vcf/chr${chr}_all_samples_ibd_hwe_0.001.vcf"
+  VCF_FILE="/home/genevieve.roberts-umw/pre_imputation_vcf/chr${chr}_all_samples_ibd_hwe_0.001.vcf"
+
+  if [[ -f "$VCF_FILE" ]]; then
+    # Update chromosome codes
+    awk '
+      BEGIN { OFS="\t" }
+      !/^##/ {
+        if ($1 ~ /^[0-9]+$/) $1 = "chr" $1;
+        else if ($1 == "23") $1 = "chrX";
+        else if ($1 == "24") $1 = "chrY";
+        else if ($1 == "26") $1 = "chrMT";
+        print
+      }
+      /^##/ { print }
+    ' "$VCF_FILE" > "${VCF_FILE}.tmp" && mv "${VCF_FILE}.tmp" "$VCF_FILE"
+
+    # bgzip the updated VCF
+    bgzip "$VCF_FILE"
   else
     echo "Error: Missing output file for chr${chr}" >&2
   fi
