b164958053a8ec8edbe0de35ede88bccd4c71a01
Genevieve Roberts <gen@gen-omix.co>
2025-06-30 18:27:13 -0400

    switch to plink merge instead of bcftools

--- impute_SNPs/3_combine_imputed_vcfs.sh
+++ impute_SNPs/3_combine_imputed_vcfs.sh
@@ -1,130 +1,149 @@
 #!/bin/bash
-#BSUB -J combine_imp_vcfs
+#BSUB -J combine_imp_pgen
 #BSUB -R "rusage[mem=8192]"
-#BSUB -o combine_imp_vcfs.out
-#BSUB -e combine_imp_vcfs.err
+#BSUB -o combine_imp_pgen.out
+#BSUB -e combine_imp_pgen.err
 #BSUB -q short
 #BSUB -n 1
 
 # Load necessary modules
+module load plink2/alpha6.1amd
+module load bcftools
 module load htslib
 
-#change working directory
 cd /home/genevieve.roberts-umw/imputed_genotypes
 
-# Output directory
 OUTPUT_DIR="./combined_output"
 mkdir -p "$OUTPUT_DIR"
 
-#make a temporary directory
-TMP_DIR="/home/genevieve.roberts-umw/imputed_genotypes/imputation_tmp"
+TMP_DIR="./imputation_tmp"
 mkdir -p "$TMP_DIR"
 
-# Imputed sources
 INPUT_DIRS=("1KG" "HRC" "MHC_alleles")
 
-# Loop over chromosomes
 for chr in {21..22}; do
     echo "Processing chromosome $chr..."
 
-    ANNOTATED_VCFS=()
+    PGEN_BASENAMES=()
+    R2_SUMMARY_FILES=()
 
-    # Step 1: Create site-only VCFs from each panel with SOURCE tag
+    # Step 1: Convert VCFs to plink2 pgen format with allele renaming
     for src in "${INPUT_DIRS[@]}"; do
         INPUT_VCF="${src}/chr${chr}.dose.vcf.gz"
         if [[ -f "$INPUT_VCF" ]]; then
-            SITE_ONLY="${TMP_DIR}/${src}_chr${chr}_site.vcf.gz"
-            MODIFIED_SITE="${TMP_DIR}/${src}_chr${chr}_site_modified.vcf.gz"
-            TAGGED_VCF="${TMP_DIR}/${src}_chr${chr}_tagged.vcf.gz"
+            OUT_PREFIX="${src}_chr${chr}"
 
-            # Strip genotypes and compress
-            bcftools view -G "$INPUT_VCF" -Oz -o "$SITE_ONLY"
-            tabix -f "$SITE_ONLY"
+            # Step 1a: Extract sample names and build FID/IID map
+            bcftools query -l "$INPUT_VCF" | awk -F'_' '{
+                old_fid = 0;
+                old_iid = $0;
+                new_fid = $1"_"$2;
+                new_iid = "";
+                for (i = 3; i <= NF; i++) {
+                    new_iid = (new_iid == "") ? $i : new_iid"_"$i;
+                }
+                print old_fid, old_iid, new_fid, new_iid;
+            }' > "${TMP_DIR}/${src}_chr${chr}_samples.txt"
 
-            # Add SOURCE tag inline
-            gunzip -c "$SITE_ONLY" | \
-                sed "s/IMPUTED;/IMPUTED;SOURCE=${src};/" | \
-                bgzip -c > "$MODIFIED_SITE"
-            tabix -f "$MODIFIED_SITE"
+            # Step 1b: Convert to PGEN with FID/IID assignment
+            plink2 --vcf "$INPUT_VCF" \
+            --extract-if-info "R2 >= 0.3" \
+            --set-all-var-ids '@:#:$r:$a' \
+            --new-id-max-allele-len 10 missing \
+            --update-ids "${TMP_DIR}/${src}_chr${chr}_samples.txt" \
+            --make-pgen --out "${TMP_DIR}/${OUT_PREFIX}"
 
-            # Add header for SOURCE tag
-            bcftools annotate \
-                -h <(echo '##INFO=<ID=SOURCE,Number=1,Type=String,Description="Source imputation panel">') \
-                "$MODIFIED_SITE" -Oz -o "$TAGGED_VCF"
-            tabix -f "$TAGGED_VCF"
+            PGEN_BASENAMES+=("${TMP_DIR}/${OUT_PREFIX}")
 
-            ANNOTATED_VCFS+=("$TAGGED_VCF")
+            # Step 1c: Save variant R2 values for later merging
+            awk -v src="$src" '
+            BEGIN { OFS = "\t" }
+            !/^#/ {
+                info = $6
+                split(info, fields, ";")
+                r2 = -1
+                for (i in fields) {
+                    if (fields[i] ~ /^R2=/) {
+                        split(fields[i], kv, "=")
+                        r2 = kv[2] + 0
+                    }
+                }
+                if (r2 >= 0.3 && $3 != ".") {
+                    print $3, r2, src
+                }
+            }' "${TMP_DIR}/${OUT_PREFIX}.pvar" > "${TMP_DIR}/${OUT_PREFIX}_variant_r2.tsv"
         else
-            echo "⚠️ Warning: missing $INPUT_VCF"
+            echo "Warning: missing $INPUT_VCF"
         fi
     done
 
-    # Step 2: Concatenate all site-only VCFs
-    CONCAT_VCF="${TMP_DIR}/chr${chr}_concat.vcf.gz"
-    bcftools concat -a -Oz -o "$CONCAT_VCF" "${ANNOTATED_VCFS[@]}"
-    tabix -f "$CONCAT_VCF"
+    # Step 3: Find best-R² variant per site
+    BEST_R2_FILE="${TMP_DIR}/chr${chr}_best_r2.tsv"
 
-    # Step 3: Pick best-R² per site
-    BEST_UNSORTED="${TMP_DIR}/chr${chr}.best_unsorted.vcf"
-    BEST_SITES="${TMP_DIR}/chr${chr}.best_sites.vcf.gz"
-
-    bcftools view "$CONCAT_VCF" | \
+    R2_SUMMARY_FILES=($(ls "${TMP_DIR}"/*_variant_r2.tsv))
+    
+    cat "${R2_SUMMARY_FILES[@]}" | \
     awk '
-    BEGIN {FS="\t"; OFS="\t"}
-    /^#/ {print; next}
+    BEGIN { OFS = "\t" }
     {
-        key = $1":"$2":"$4":"$5
-        match($8, /R2=([^;]+)/, arr)
-        r2 = (arr[1] != "") ? arr[1] + 0 : 0
-        if (r2 < 0.3) next
-        if (!(key in best) || r2 > best_r2[key]) {
-            best[key] = $0
-            best_r2[key] = r2
+        key = $1
+        if (key in best_r2) {
+            if ($2 > best_r2[key]) {
+                best_r2[key] = $2
+                best_variant[key] = $0
+            }
+        } else {
+            best_r2[key] = $2
+            best_variant[key] = $0
         }
     }
     END {
-        for (k in best) print best[k]
-    }' > "$BEST_UNSORTED"
+        for (k in best_variant) {
+            print best_variant[k]
+        }
+    }' > "${BEST_R2_FILE}"
+    sort "${BEST_R2_FILE}" -o "${BEST_R2_FILE}"
 
-    # Now compress sorted output
-    bcftools sort "$BEST_UNSORTED" -Oz -o "$BEST_SITES"
-    tabix -f "$BEST_SITES"
+    # Step 4: Extract variant list (plink2 ID format) and make filtered pgens
+    FILTERED_PGENS=()
 
-
-    # Step 4: Extract genotypes from correct original panel using SOURCE filter
-    GENOTYPED_VCFS=()
     for src in "${INPUT_DIRS[@]}"; do
-        ORIG_VCF="${src}/chr${chr}.dose.vcf.gz"
-        SRC_SITES="${TMP_DIR}/${src}_chr${chr}_best_sites.vcf.gz"
+        OUT_PREFIX="${src}_chr${chr}"
+        if [[ -f "${TMP_DIR}/${OUT_PREFIX}.pgen" ]]; then
+            #only include files that have a pgen in the temp dir
+            PGEN_BASENAMES+=("${TMP_DIR}/${OUT_PREFIX}")
 
-        # Filter best_sites for this panel
-        bcftools view -i "INFO/SOURCE == \"${src}\"" "$BEST_SITES" -Oz -o "$SRC_SITES"
-        tabix -f "$SRC_SITES"
+            #make the extract variant list
+            VARIANT_LIST="${TMP_DIR}/${OUT_PREFIX}_filtered_variants.txt"
+            grep "${src}" "${BEST_R2_FILE}" > "$VARIANT_LIST" 
 
-        if [[ -s "$SRC_SITES" && -f "$ORIG_VCF" ]]; then
-            OUT_VCF="${TMP_DIR}/${src}_chr${chr}_geno.vcf.gz"
-            bcftools view -R "$SRC_SITES" "$ORIG_VCF" -Oz -o "$OUT_VCF"
-            tabix -f "$OUT_VCF"
-            GENOTYPED_VCFS+=("$OUT_VCF")
+            #make the filtered pgen for each imputation source
+            plink2 --pfile "${PGEN_BASENAMES[0]}" \
+                --extract "$VARIANT_LIST" \
+                --make-pgen --out "${TMP_DIR}/${OUT_PREFIX}_filtered"
+            
+            FILTERED_PGENS+=("${TMP_DIR}/${OUT_PREFIX}_filtered")
+        else
+            echo "Warning: missing ${TMP_DIR}/${OUT_PREFIX}.pgen"
+            continue
         fi
     done
+    
+    # Step 5: Merge all filtered pgens
+    MERGE_LIST="${TMP_DIR}/chr${chr}_merge_list.txt"
+    > "$MERGE_LIST"
+    for f in "${FILTERED_PGENS[@]:1}"; do
+        echo "$f" >> "$MERGE_LIST"
+    done
 
-    # Step 5: Merge all genotype files (some samples may be missing from some panels)
-    MERGED_VCF="${TMP_DIR}/chr${chr}_merged_geno.vcf.gz"
-    bcftools merge --force-samples --missing-to-na -Oz -o "$MERGED_VCF" "${GENOTYPED_VCFS[@]}"
-    tabix -f "$MERGED_VCF"
+    echo "Files to be merged for chr${chr}:"
+    cat "$MERGE_LIST"
 
-    # Step 6: Annotate merged genotypes with SOURCE tag
-    FINAL_VCF="${OUTPUT_DIR}/chr${chr}.best_imputed.vcf.gz"
-    bcftools annotate -a "$BEST_SITES" -c CHROM,POS,ID,REF,ALT,INFO/SOURCE \
-        "$MERGED_VCF" -Oz -o "$FINAL_VCF"
-    tabix -f "$FINAL_VCF"
+    MERGED_PREFIX="${OUTPUT_DIR}/chr${chr}_merged"
 
-    echo "Done: $FINAL_VCF"
+    plink2 --pfile "${FILTERED_PGENS[0]}" \
+        --pmerge-list "$MERGE_LIST" \
+        --make-pgen --out "$MERGED_PREFIX"
+
+    echo "Finished: ${MERGED_PREFIX}.pgen"
 done
-
-# Optional: merge all chromosomes
-bcftools merge -a ${OUTPUT_DIR}/chr*.best_imputed.vcf.gz -Oz -o imputed_snps_combined.vcf.gz
-tabix -f imputed_snps_combined.vcf.gz
-
-echo "Final output: imputed_snps_combined.vcf.gz"
