045694724df5c648862a747607603cc97a891055
Genevieve Roberts <gen@gen-omix.co>
2025-07-25 20:58:05 -0400

    rm "chr" prefix from ref fasta & add download info comments

--- impute_SNPs/4_liftover_to_hg38.sh
+++ impute_SNPs/4_liftover_to_hg38.sh
@@ -8,11 +8,27 @@
 #BSUB -n 1
 
 module load picard/3.1.1
+# module load samtools/1.3
 
-# Run Picard LiftoverVcf
+## Get the chain file
+#wget https://hgdownload.soe.ucsc.edu/goldenPath/hg19/liftOver/hg19ToHg38.over.chain.gz
+#gunzip hg19ToHg38.over.chain.gz
+
+## Get the reference genome
+#wget http://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/hg38.fa.gz
+#gunzip hg38.fa.gz
+
+## Remove chr prefix from hg38 reference genome
+#sed 's/^>chr/>/' hg38.fa > hg38_nochr.fa
+
+## Make the dictionary and index for the hg38 reference genome
+#java -jar $PICARDJAR CreateSequenceDictionary R=hg38_nochr.fa O=hg38_nochr.dict
+# samtools faidx hg38_nochr.fa
+
+## Run Picard LiftoverVcf
 java -jar $PICARDJAR LiftoverVcf \
     I=/home/genevieve.roberts-umw/imputed_genotypes/1KG/test_chr22.dose.vcf \
     O=/home/genevieve.roberts-umw/imputed_genotypes/1KG/test_hg38_chr22.dose.vcf \
     CHAIN=/home/genevieve.roberts-umw/liftover_chain/hg19ToHg38.over.chain \
     REJECT=/home/genevieve.roberts-umw/imputed_genotypes/1KG/test_hg38_rejected_variants.vcf \
-    R=/home/genevieve.roberts-umw/liftover_chain/hg38.fa
+    R=/home/genevieve.roberts-umw/liftover_chain/hg38_nochr.fa
