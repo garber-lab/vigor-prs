b290aa170b349195b9c81580b0617a22c591a67c
Genevieve Roberts <gen@gen-omix.co>
2025-07-28 15:22:49 -0400

    update vcf contig names to "chr" prefix instead of chain or ref

--- impute_SNPs/4_liftover_to_hg38.sh
+++ impute_SNPs/4_liftover_to_hg38.sh
@@ -8,7 +8,43 @@
 #BSUB -n 1
 
 module load picard/3.1.1
-# module load samtools/1.3
+module load bcftools/1.16
+
+# ---- PARAMETERS ----
+CHAIN=/home/genevieve.roberts-umw/liftover_chain/hg19ToHg38.over.chain
+REFERENCE=/home/genevieve.roberts-umw/liftover_chain/hg38.fa
+
+IN_DIR=/home/genevieve.roberts-umw/imputed_genotypes/1KG
+OUT_DIR=/home/genevieve.roberts-umw/imputed_genotypes/1KG
+CHR=22
+VCF_PREFIX=test_chr${CHR}
+
+# ---- FILE PATHS ----
+IN_VCF=${IN_DIR}/${VCF_PREFIX}.dose.vcf
+OUT_VCF_WITH_CHR=${OUT_DIR}/${VCF_PREFIX}_addchr.dose.vcf.gz
+REJECT_VCF=${OUT_DIR}/test_hg38_chr${CHR}_rejected_variants.vcf
+LIFTED_VCF=${OUT_DIR}/test_hg38_chr${CHR}.dose.vcf
+
+# ---- STEP 1: Add chr prefixes to contigs ----
+echo "Adding 'chr' prefixes to contigs for chr${CHR}..."
+bcftools annotate \
+  --rename-chrs <(echo -e "${CHR}\tchr${CHR}") \
+  -O z -o "$OUT_VCF_WITH_CHR" "$IN_VCF"
+
+bcftools index "$OUT_VCF_WITH_CHR"
+
+# ---- STEP 2: Run Picard LiftoverVcf ----
+echo "Running LiftoverVcf for chr${CHR}..."
+java -jar $PICARDJAR LiftoverVcf \
+    I="$OUT_VCF_WITH_CHR" \
+    O="$LIFTED_VCF" \
+    CHAIN="$CHAIN" \
+    REJECT="$REJECT_VCF" \
+    R="$REFERENCE"
+
+echo "Liftover complete for chr${CHR}."
+
+#module load samtools/1.3
 
 ## Get the chain file
 #wget https://hgdownload.soe.ucsc.edu/goldenPath/hg19/liftOver/hg19ToHg38.over.chain.gz
@@ -18,17 +54,6 @@ module load picard/3.1.1
 #wget http://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/hg38.fa.gz
 #gunzip hg38.fa.gz
 
-## Remove chr prefix from hg38 reference genome
-#sed 's/^>chr/>/' hg38.fa > hg38_nochr.fa
-
 ## Make the dictionary and index for the hg38 reference genome
-#java -jar $PICARDJAR CreateSequenceDictionary R=hg38_nochr.fa O=hg38_nochr.dict
-# samtools faidx hg38_nochr.fa
-
-## Run Picard LiftoverVcf
-java -jar $PICARDJAR LiftoverVcf \
-    I=/home/genevieve.roberts-umw/imputed_genotypes/1KG/test_chr22.dose.vcf \
-    O=/home/genevieve.roberts-umw/imputed_genotypes/1KG/test_hg38_chr22.dose.vcf \
-    CHAIN=/home/genevieve.roberts-umw/liftover_chain/hg19ToHg38.over.chain \
-    REJECT=/home/genevieve.roberts-umw/imputed_genotypes/1KG/test_hg38_rejected_variants.vcf \
-    R=/home/genevieve.roberts-umw/liftover_chain/hg38_nochr.fa
+#java -jar $PICARDJAR CreateSequenceDictionary R=hg38.fa O=hg38.dict
+# samtools faidx hg38.fa
