993b277467a28efd1ea38d7724bcad27e32f90ec
Genevieve Roberts <gen@gen-omix.co>
2025-07-28 21:29:09 -0400

    working liftover, but only 1000 snps

--- impute_SNPs/4_liftover_to_hg38.sh
+++ impute_SNPs/4_liftover_to_hg38.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #BSUB -J liftover_chr22
-#BSUB -R "rusage[mem=96000]"
+#BSUB -R "rusage[mem=128000]"
 #BSUB -o liftover_chr22.out
 #BSUB -e liftover_chr22.err
 #BSUB -q short
-#BSUB -W 1:00
+#BSUB -W 0:30
 #BSUB -n 1
 
 module load picard/3.1.1
@@ -21,21 +21,26 @@ VCF_PREFIX=test_chr${CHR}
 
 # ---- FILE PATHS ----
 IN_VCF=${IN_DIR}/${VCF_PREFIX}.dose.vcf
-OUT_VCF_WITH_CHR=${OUT_DIR}/${VCF_PREFIX}_addchr.dose.vcf.gz
-REJECT_VCF=${OUT_DIR}/test_hg38_chr${CHR}_rejected_variants.vcf
-LIFTED_VCF=${OUT_DIR}/test_hg38_chr${CHR}.dose.vcf
+VCF_1000=${OUT_DIR}/${VCF_PREFIX}_first1000.dose.vcf
+OUT_VCF_WITH_CHR=${OUT_DIR}/${VCF_PREFIX}_first1000_addchr.dose.vcf.gz
+REJECT_VCF=${OUT_DIR}/test_hg38_chr${CHR}_first1000_rejected.vcf
+LIFTED_VCF=${OUT_DIR}/test_hg38_chr${CHR}_first1000.dose.vcf
+
+# ---- STEP 0: Subset first 1000 SNPs ----
+echo "Subsetting first 1000 records for chr${CHR}..."
+(bcftools view -h "$IN_VCF" && bcftools view -H "$IN_VCF" | head -n 1000) > "$VCF_1000"
 
 # ---- STEP 1: Add chr prefixes to contigs ----
-echo "Adding 'chr' prefixes to contigs for chr${CHR}..."
+echo "Adding 'chr' prefixes to contigs..."
 bcftools annotate \
   --rename-chrs <(echo -e "${CHR}\tchr${CHR}") \
-  -O z -o "$OUT_VCF_WITH_CHR" "$IN_VCF"
+  -O z -o "$OUT_VCF_WITH_CHR" "$VCF_1000"
 
 bcftools index "$OUT_VCF_WITH_CHR"
 
 # ---- STEP 2: Run Picard LiftoverVcf ----
-echo "Running LiftoverVcf for chr${CHR}..."
-JAVA_MEM=90g
+echo "Running LiftoverVcf..."
+JAVA_MEM=120g
 java -Xmx$JAVA_MEM -jar $PICARDJAR LiftoverVcf \
     I="$OUT_VCF_WITH_CHR" \
     O="$LIFTED_VCF" \
@@ -44,18 +49,4 @@ java -Xmx$JAVA_MEM -jar $PICARDJAR LiftoverVcf \
     R="$REFERENCE" \
     MAX_RECORDS_IN_RAM=1000000
 
-echo "Liftover complete for chr${CHR}."
-
-#module load samtools/1.3
-
-## Get the chain file
-#wget https://hgdownload.soe.ucsc.edu/goldenPath/hg19/liftOver/hg19ToHg38.over.chain.gz
-#gunzip hg19ToHg38.over.chain.gz
-
-## Get the reference genome
-#wget http://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/hg38.fa.gz
-#gunzip hg38.fa.gz
-
-## Make the dictionary and index for the hg38 reference genome
-#java -jar $PICARDJAR CreateSequenceDictionary R=hg38.fa O=hg38.dict
-# samtools faidx hg38.fa
+echo "Liftover complete."
