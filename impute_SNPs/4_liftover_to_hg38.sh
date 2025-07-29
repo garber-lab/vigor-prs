b7dea09f29090ac3c93ae5621a4408a7e8a5c3a5
Genevieve Roberts <gen@gen-omix.co>
2025-07-28 23:30:32 -0400

    liftover working

--- impute_SNPs/4_liftover_to_hg38.sh
+++ impute_SNPs/4_liftover_to_hg38.sh
@@ -22,34 +22,30 @@ VCF_PREFIX=chr${CHR}_imputed_combined
 
 # ---- FILE PATHS ----
 IN_VCF=${IN_DIR}/${VCF_PREFIX}.vcf.gz
-VCF_1000=${OUT_DIR}/${VCF_PREFIX}_first1000.vcf
-VCF_1000_GZ=${VCF_1000}.gz
-OUT_VCF_WITH_CHR=${OUT_DIR}/${VCF_PREFIX}_first1000_addchr.dose.vcf.gz
-REJECT_VCF=${OUT_DIR}/test_hg38_chr${CHR}_first1000_rejected.vcf.gz
-LIFTED_VCF=${OUT_DIR}/test_hg38_chr${CHR}_first1000.dose.vcf.gz
+CLEANED_VCF=${OUT_DIR}/tmp_${VCF_PREFIX}_cleaned.vcf.gz
+VCF_WITH_CHR=${OUT_DIR}/tmp_${VCF_PREFIX}_addchr.vcf.gz
+REJECT_VCF=${OUT_DIR}/tmp_grch38_chr${CHR}_rejected.vcf.gz
+LIFTED_VCF=${OUT_DIR}/grch38_chr${CHR}.vcf.gz
 
-# ---- STEP 0: Subset first 1000 records for chr${CHR} ----
-echo "Subsetting first 1000 records for chr${CHR}..."
-# Save as uncompressed first
-(bcftools view -h "$IN_VCF" && bcftools view -H "$IN_VCF" | head -n 1000) > "$VCF_1000"
-bgzip -f "$VCF_1000"  # compress to .vcf.gz
-tabix -f "$VCF_1000_GZ"  # index
+# ---- STEP 1: Remove symbolic reference alleles ----
+echo "Filtering out symbolic reference alleles like <CN0> from REF field..."
+bcftools view -e 'REF ~ "^<"' "$IN_VCF" -O z -o "$CLEANED_VCF"
+bcftools index -f "$CLEANED_VCF"
 
-# ---- STEP 1: Add 'chr' prefix to contig name ----
+# ---- STEP 2: Add 'chr' prefix to contig ----
 echo "Adding 'chr' prefix to contig ${CHR}..."
 bcftools annotate \
   --rename-chrs <(echo -e "${CHR}\tchr${CHR}") \
-  -O z -o "$OUT_VCF_WITH_CHR" "$VCF_1000_GZ"
-bcftools index -f "$OUT_VCF_WITH_CHR"
+  -O z -o "$VCF_WITH_CHR" "$CLEANED_VCF"
+bcftools index -f "$VCF_WITH_CHR"
 
-# ---- STEP 2: Run Picard LiftoverVcf ----
+# ---- STEP 3: Run Picard LiftoverVcf ----
 echo "Running Picard LiftoverVcf..."
 JAVA_MEM=120g
 PICARD_OPTIONS="-Xmx$JAVA_MEM"
 
-# Run liftover
 java $PICARD_OPTIONS -jar $PICARDJAR LiftoverVcf \
-  I="$OUT_VCF_WITH_CHR" \
+  I="$VCF_WITH_CHR" \
   O="$LIFTED_VCF" \
   CHAIN="$CHAIN" \
   REJECT="$REJECT_VCF" \
@@ -57,5 +53,8 @@ java $PICARD_OPTIONS -jar $PICARDJAR LiftoverVcf \
   MAX_RECORDS_IN_RAM=1000000 \
   CREATE_INDEX=true \
   VALIDATION_STRINGENCY=LENIENT
+
+# Remove temporary files
+rm -f "$CLEANED_VCF" "$VCF_WITH_CHR" "$REJECT_VCF"
 
 echo "Liftover complete."
